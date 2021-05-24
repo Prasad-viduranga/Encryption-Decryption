b65127107dbac495310c090860d2847d6091b3b1
Prasad Viduranga <pkpviduranga@gmail.com>
2021-05-24 10:15:56 +0530

    Create button for navigate the secondForm

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -1,14 +1,23 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainFormController {
     public TextField txtText;
     public TextField txtKey;
     public Button btnEncrypt;
+    public AnchorPane contextOfMainForm;
 
-    public void btnEncryptOnAction(ActionEvent actionEvent) {
+    public void btnEncryptOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) contextOfMainForm.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/SecondForm.fxml"))));
     }
 }
--- src/controller/SecondFormController.java
+++ src/controller/SecondFormController.java
@@ -3,11 +3,14 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class SecondFormController {
     public TextField txtCipherText;
     public TextField txtKey;
     public Button btnDecrypt;
+    public AnchorPane contextOfMainForm;
+    public AnchorPane contextOfSecondForm;
 
     public void btnDecryptOnAction(ActionEvent actionEvent) {
     }
