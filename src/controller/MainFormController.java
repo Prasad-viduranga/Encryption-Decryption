8f669978c9eebf1e30ce7a6a63eb3098afe31e9d
Prasad Viduranga <pkpviduranga@gmail.com>
2021-05-24 10:04:16 +0530

    Created main interface

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -1,4 +1,14 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class MainFormController {
+    public TextField txtText;
+    public TextField txtKey;
+    public Button btnEncrypt;
+
+    public void btnEncryptOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/controller/SecondFormController.java
+++ src/controller/SecondFormController.java
@@ -1,4 +1,14 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class SecondFormController {
+    public TextField txtCipherText;
+    public TextField txtKey;
+    public Button btnDecrypt;
+
+    public void btnDecryptOnAction(ActionEvent actionEvent) {
+    }
 }
